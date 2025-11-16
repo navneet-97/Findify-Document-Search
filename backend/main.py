c96d2c02c912c78794e6e4ceb881d01f847a5b26
navneet-97 <navneet-97@users.noreply.github.com>
2025-11-16 17:39:43 +0530

    "changes"

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,4 @@ import os
 from server import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))

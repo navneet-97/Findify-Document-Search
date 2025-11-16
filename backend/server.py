84b75de5fbcaf2317c52f43997c306dc114eae65
navneet-97 <navneet-97@users.noreply.github.com>
2025-11-16 14:03:53 +0530

    "changes"

--- backend/server.py
+++ backend/server.py
@@ -520,4 +520,4 @@ async def shutdown_db_client():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=False)
